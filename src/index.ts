7cc265f8177ebdb9cc5dfa128f494aec95d12162
utkarsh bhaskar <utkarshbhaskar05@gmail.com>
2022-01-20 20:55:44 +0530

    converted to version 3

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,17 @@
 import express from "express";
+import { ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core';
 import bodyParser from 'body-parser';
 import "reflect-metadata";
 import {createConnection} from 'typeorm';
-import { ApolloServer } from "apollo-server-express";
+import {ApolloServer} from "apollo-server-express";
 import  typeDefs  from "../src/schema/typeDefs/typedef";
 import  resolvers  from "../src/schema/resolvers/resolver";
-require("dotenv").config();
+//require("dotenv").config();
+//import { applyMiddleware } from "graphql-middleware";
 import http from "http";
 import cors from "cors";
 
 
-
-const app=express();
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-
-app.use(cors());
 
 const postgres = () => {
     return createConnection({
@@ -29,41 +25,57 @@ const postgres = () => {
     });
 }
 
-const server = new ApolloServer({ typeDefs, resolvers,introspection: true });
-server.applyMiddleware({app,path: "/graphql", cors: false});
+
+const app = express();
+
+// app.use((_,res)=>{
+//   res.send("Hello from apollo server");
+// });
 
 
 
-postgres().then(async ()=>{
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
-    console.log(`database synced!`);
+app.use(cors());
 
-}).catch((err)=>{
-  console.error(err);
-});
+const server = new ApolloServer({ typeDefs, resolvers,
+  plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+  introspection: true});
+
+//server.applyMiddleware({app,path: "/graphql", cors: false});
 
 
-app.use((_,res)=>{
-    res.send("Hello from apollo server");
-});
+
+// postgres().then(async ()=>{
+
+//     console.log(`database synced!`);
+
+// }).catch((err)=>{
+//   console.error(err);
+// });
+
+
 
 
   const httpServer = http.createServer(app);
   
-  httpServer.listen({ port: 3000 }, () => {
-    console.log(
-      `Apollo server ready at http://localhost:3000/graphql`
-    );
-  });
+//   httpServer.listen({ port: 3000 }, () => {
+//     console.log(
+//       `Apollo server ready at http://localhost:3000/graphql`
+//     );
+//   });
 
 
-// postgres()
-//   .then(async () => {
-//     console.log("Postgres Connected!!");
-//     server.applyMiddleware({ app });
 
-//     httpServer.listen(4000, async () => {
-//       console.log("Server started on localhost:4000");
-//     });
-//   })
-//   .catch((error) => console.error(error));
+postgres()
+  .then(async () => {
+    console.log("Postgres Connected!!");
+    await server.start();
+    server.applyMiddleware({ app,path: "/graphql"});
+
+    httpServer.listen(4000, async () => {
+      console.log("Server started on localhost:4000");
+    });
+  })
+  .catch((error) => console.error(error));
